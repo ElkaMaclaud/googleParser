4efb9629e32914ab7362a6158371f93c3a55a2c2
ElkaMaclaud <Elkamaclaud@gmail.com>
2024-09-11 19:59:25 +0300

    Fixed logic for getting list of unclicked nodelist elements.

--- index_2.js
+++ index_2.js
@@ -12,7 +12,10 @@ const mapsParser = async (req) => {
     });
     await page.setViewport({ width: 1480, height: 1000 });
 
-    await page.waitForSelector("input", { waitUntil: 3000 });
+    await page.waitForSelector("input", {
+      waitUntil: "visible",
+      timeout: 3000,
+    });
 
     await page.click("input");
     await page.type("input", req, { delay: 200 });
@@ -24,76 +27,92 @@ const mapsParser = async (req) => {
     await page.mouse.wheel({ deltaY: 1500 });
 
     await page.waitForSelector(".Nv2PK", { visible: true, timeout: 2000 });
+    const clickedElements = new Set();
 
     const allData = { data: [] };
-    const clickedElements = new Set();
     const uniqueData = new Set();
-    let startIndex = 0;
+
     const scrollableElement = await page.waitForSelector("#QA0Szd .QjC7t", {
       visible: true,
       timeout: 2000,
     });
+    let filteredArr = [];
 
     while (true) {
       const findList = await page.$$(".Nv2PK");
-      const arr = Array.from(findList).slice(startIndex);
-      console.log("////////////////////////", arr.length);
+      filteredArr = []
+     
+      for (let li of findList) {
+        const elementId = await page.evaluate((el) => el.outerHTML, li);
+        if (!clickedElements.has(elementId)) {
+          console.log("1111111111111", )
+          filteredArr.push(li); 
+        }
+      }
+      console.log(
+        "////////////////////////",
+        filteredArr.length,
+        findList.length
+      );
 
-      startIndex += arr.length;
-      if (!arr.length) {
+      if (!filteredArr.length) {
         break;
       }
 
-      for (let li of arr) {
+      for (let i = 0; i < filteredArr.length; i ++) {
+        const li = filteredArr[i]
         const elementId = await page.evaluate((el) => el.outerHTML, li);
-        if (!clickedElements.has(elementId)) {
+        let clickSuccess = false;
+        clickedElements.add(elementId);
+        for (let attempt = 0; attempt < 3; attempt++) {
           try {
-            await new Promise((resolve) => setTimeout(resolve, 2500));
-            try {
-              await li.click();
-            } catch (error) {
-              console.error('Ошибка при КЛИКЕ на элемент:', error);
-          }   
-            clickedElements.add(elementId);
-
-            await page.waitForSelector(
-              "#QA0Szd div.w6VYqd > div.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.XiKgde",
-              { visible: true, timeout: 3000 }
-            );
-            const element = await page.$(
-              "#QA0Szd div.w6VYqd > div.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.XiKgde"
-            )
-
-            if (element) {
-              const result = await page.evaluate((el) => {
-                const name = el.querySelector("h1");
-                const adress = el.querySelector("[data-item-id='address']");
-                const website = el.querySelector("[data-item-id='authority']");
-                const phone = el.querySelector("[data-item-id^='phone:tel:']");
-                return {
-                  name: name ? name.innerText : "",
-                  adress: adress ? adress.innerText : "",
-                  website: website ? website.href : "",
-                  phone: phone
-                    ? phone
-                        .getAttribute("data-item-id")
-                        .replace("phone:tel:", "")
-                    : "",
-                };
-              }, element);
-
-              const uniqueKey =
-                result.name + result.adress + result.website + result.phone;
-              if (!uniqueData.has(uniqueKey)) {
-                uniqueData.add(uniqueKey);
-                allData.data.push(result);
-              }
-            }
+            await new Promise((resolve) => setTimeout(resolve, 3500));
+            await li.click();
+            clickSuccess = true;
+            break;
           } catch (error) {
-            console.error("Ошибка при обработке элемента:", li, li.innerText, arr.length, error);
+            console.error("Ошибка при КЛИКЕ на элемент:", error);
+            if (attempt === 1) {
+              console.error("Не удалось кликнуть на элемент после 3 попыток.");
+            }
+          }
+        }
+
+        if (clickSuccess) {
+          await page.waitForSelector(
+            "#QA0Szd div.w6VYqd > div.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.XiKgde",
+            { visible: true, timeout: 2000 }
+          );
+          const element = await page.$(
+            "#QA0Szd div.w6VYqd > div.Hu9e2e.tTVLSc > div > div.e07Vkf.kA9KIf > div > div > div.XiKgde"
+          );
+
+          if (element) {
+            const result = await page.evaluate((el) => {
+              const name = el.querySelector("h1");
+              const adress = el.querySelector("[data-item-id='address']");
+              const website = el.querySelector("[data-item-id='authority']");
+              const phone = el.querySelector("[data-item-id^='phone:tel:']");
+              return {
+                name: name ? name.innerText : "",
+                adress: adress ? adress.innerText : "",
+                website: website ? website.href : "",
+                phone: phone
+                  ? phone.getAttribute("data-item-id").replace("phone:tel:", "")
+                  : "",
+              };
+            }, element);
+
+            const uniqueKey =
+              result.name + result.adress + result.website + result.phone;
+            if (!uniqueData.has(uniqueKey)) {
+              uniqueData.add(uniqueKey);
+              allData.data.push(result);
+            }
           }
         }
       }
+
       await page.evaluate((elScr) => {
         if (elScr) {
           elScr.scrollBy(0, 1500);
